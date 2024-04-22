2c171ca73ed5de6178a05dd9b266b6d94f89c42c
Javier <jmv81535@gmail.com>
2024-04-22 21:42:11 +0200

    Back project added

---
+++ Backend/siugym/src/main/java/com/javiermv/siugym/SiugymApplication.java
@@ -0,0 +1,13 @@
+package com.javiermv.siugym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiugymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiugymApplication.class, args);
+	}
+
+}
---
+++ Backend/siugym/src/test/java/com/javiermv/siugym/SiugymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiermv.siugym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiugymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
